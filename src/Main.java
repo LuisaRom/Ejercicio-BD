813607d61aacfe871057b0f5189507abda317f9b
Luisa Romero <lufromero13@gmail.com>
2024-09-06 17:04:52 -0500

    RamaPrueba

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        double numero1, numero2, suma, resta, multi;
+        double numero1, numero2, suma, resta, multi, raiz1, raiz2, seno, coseno;
 
         System.out.print("INGRESE EL PRIMER VALOR");
         numero1 = teclado.nextDouble();
@@ -17,9 +17,18 @@ public class Main {
         suma = numero1+numero2;
         resta = numero1-numero2;
         multi= numero1*numero2;
+        raiz1= Math.sqrt(numero1);
+        raiz2 = Math.sqrt(numero2);
+        seno= Math.sqrt(numero1);
+        coseno= Math.sqrt(numero2);
 
         System.out.println("La suma es "+suma);
         System.out.println("La resta es: "+ resta);
         System.out.println("La multiplicacion es: "+multi);
+        System.out.println("La Raiz 1 es:"+raiz1);
+        System.out.println("La Raiz 2 es:"+raiz2);
+        System.out.println("El seno es:"+seno);
+        System.out.println("El coseno es:"+coseno);
+
         }
 }
